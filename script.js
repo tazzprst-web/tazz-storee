c4e638c702f9b81099fa32dd992717aff0a9e7f1
tazzprst-web <tazzprst@gmail.com>
2025-09-27 13:24:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Handle submit form
+document.getElementById("orderForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Pesanan kamu sudah dikirim ke admin (085709518077). Silakan lakukan pembayaran via Dana!");
+});
