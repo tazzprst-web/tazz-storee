f18d5590a9372c8e3bd0b9ce7ab0471a77d2998c
tazzprst-web <tazzprst@gmail.com>
2025-09-27 13:30:05 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Handle submit form
-document.getElementById("orderForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Pesanan kamu sudah dikirim ke admin (085709518077). Silakan lakukan pembayaran via Dana!");
-});
