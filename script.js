668036185dba3fe65ae28eda437bf5bc00159908
tazzprst-web <tazzprst@gmail.com>
2025-11-16 19:23:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function menu1() {
+    window.location.href = "https://wa.me/6285709518077?text=min%20mau%20beli%20am%20prem/all%20apk%20prem";
+}
+
+function menu2() {
+    window.location.href = "https://wa.me/6285709518077?text=bang%20mau%20tanya%20soal%20am%20prem";
+}
+
+function menu3() {
+    window.location.href = "https://wa.me/6285709518077";
+}
